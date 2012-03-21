2d81c88c6f567abd8c422f78aaf713ce768ac74f
Uwe Trottmann <uwe.trottmann@gmail.com>
2012-03-21 15:53:08 +0100

    Use a Loader instead of an AsyncTask.

--- SeriesGuideMovies/src/com/uwetrottmann/movies/ui/TraktMoviesFragment.java
+++ SeriesGuideMovies/src/com/uwetrottmann/movies/ui/TraktMoviesFragment.java
@@ -2,27 +2,32 @@
 package com.uwetrottmann.movies.ui;
 
 import com.actionbarsherlock.app.SherlockListFragment;
+import com.jakewharton.apibuilder.ApiException;
+import com.jakewharton.trakt.TraktException;
 import com.jakewharton.trakt.entities.Movie;
 import com.uwetrottmann.movies.R;
 import com.uwetrottmann.movies.util.ImageDownloader;
 import com.uwetrottmann.movies.util.Utils;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TraktMoviesFragment extends SherlockListFragment {
+public class TraktMoviesFragment extends SherlockListFragment implements
+        LoaderCallbacks<List<Movie>> {
 
     public enum TraktCategory {
         TRENDING(0), RELEASED(1);
@@ -54,103 +59,175 @@ public class TraktMoviesFragment extends SherlockListFragment {
         String CATEGORY = "category";
     }
 
-    private TraktMovieAdapter mAdapter;
+    private static final int MOVIES_LOADER_ID = 0;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // so we don't have to do a network op each config change
-        setRetainInstance(true);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        /*
-         * never use this here (on config change the view needed before removing
-         * the fragment)
-         */
-        // if (container == null) {
-        // return null;
-        // }
-        return inflater.inflate(R.layout.moviefragment, container, false);
-    }
+    private TraktMoviesAdapter mAdapter;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        // only create and fill a new adapter if there is no previous one
-        // (e.g. after config/page changed)
-        if (mAdapter == null) {
-            mAdapter = new TraktMovieAdapter(getActivity(), R.layout.movie_row,
-                    new ArrayList<Movie>());
-
-            TraktCategory cat = TraktCategory.fromValue(getArguments().getInt(InitBundle.CATEGORY));
-            new GetTraktMoviesTask(getActivity()).execute(cat);
-        }
-    }
-
-    public class GetTraktMoviesTask extends AsyncTask<TraktCategory, Void, List<Movie>> {
-
-        private Context mContext;
-
-        public GetTraktMoviesTask(Context context) {
-            mContext = context;
-        }
-
-        @Override
-        protected List<Movie> doInBackground(TraktCategory... params) {
-            TraktCategory type = params[0];
-            List<Movie> movies = new ArrayList<Movie>();
-
-            try {
-                switch (type) {
-                    case TRENDING:
-                        movies = Utils.getServiceManager(mContext).movieService().trending().fire();
-                        break;
-                    default:
-                        movies = Utils.getServiceManager(mContext).movieService().trending().fire();
-                        break;
-                }
-            } catch (Exception e) {
-                // we don't care
-            }
-
-            return movies;
-        }
-
-        @Override
-        protected void onPostExecute(List<Movie> result) {
-            setMovieList(result);
-        }
-    }
-
-    private void setMovieList(List<Movie> results) {
-        mAdapter.clear();
-        if (Utils.isHoneycombOrHigher()) {
-            mAdapter.addAll(results);
-        } else {
-            for (Movie movie : results) {
-                mAdapter.add(movie);
-            }
-        }
+        // set list adapter
+        mAdapter = new TraktMoviesAdapter(getSherlockActivity());
         setListAdapter(mAdapter);
+
+        // style list view
+        final ListView list = getListView();
+        list.setDivider(getResources().getDrawable(R.drawable.divider_horizontal_holo_dark));
+        list.setSelector(R.drawable.list_selector_holo_dark);
+        list.setClipToPadding(Utils.isHoneycombOrHigher() ? false : true);
+        final float scale = getResources().getDisplayMetrics().density;
+        int layoutPadding = (int) (10 * scale + 0.5f);
+        int defaultPadding = (int) (8 * scale + 0.5f);
+        list.setPadding(layoutPadding, layoutPadding, layoutPadding, defaultPadding);
+
+        // nag about no connectivity
+        if (!Utils.isNetworkConnected(getActivity())) {
+            setEmptyText(getString(R.string.offline));
+            setListShown(true);
+        } else {
+            setEmptyText(getString(R.string.movies_empty));
+            setListShown(false);
+            getLoaderManager().initLoader(MOVIES_LOADER_ID, getArguments(), this);
+        }
     }
 
-    public class TraktMovieAdapter extends ArrayAdapter<Movie> {
+    @Override
+    public Loader<List<Movie>> onCreateLoader(int id, Bundle args) {
+        return new TraktMoviesLoader(getSherlockActivity());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Movie>> loader, List<Movie> data) {
+        mAdapter.setData(data);
+
+        if (isResumed()) {
+            setListShown(true);
+        } else {
+            setListShownNoAnimation(true);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Movie>> loader) {
+        mAdapter.setData(null);
+    }
+
+    private static class TraktMoviesLoader extends AsyncTaskLoader<List<Movie>> {
+
+        private List<Movie> mData;
+
+        public TraktMoviesLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        public List<Movie> loadInBackground() {
+            try {
+                return Utils.getServiceManager(getContext()).movieService().trending().fire();
+            } catch (TraktException te) {
+                return null;
+            } catch (ApiException ae) {
+                return null;
+            }
+
+        }
+
+        /**
+         * Called when there is new data to deliver to the client. The super
+         * class will take care of delivering it; the implementation here just
+         * adds a little more logic.
+         */
+        @Override
+        public void deliverResult(List<Movie> data) {
+            if (isReset()) {
+                // An async query came in while the loader is stopped. We
+                // don't need the result.
+                if (data != null) {
+                    onReleaseResources(data);
+                }
+            }
+            List<Movie> oldData = data;
+            mData = data;
+
+            if (isStarted()) {
+                // If the Loader is currently started, we can immediately
+                // deliver its results.
+                super.deliverResult(data);
+            }
+
+            if (oldData != null) {
+                onReleaseResources(oldData);
+            }
+        }
+
+        @Override
+        protected void onStartLoading() {
+            if (mData != null) {
+                deliverResult(mData);
+            } else {
+                forceLoad();
+            }
+        }
+
+        /**
+         * Handles a request to stop the Loader.
+         */
+        @Override
+        protected void onStopLoading() {
+            // Attempt to cancel the current load task if possible.
+            cancelLoad();
+        }
+
+        /**
+         * Handles a request to cancel a load.
+         */
+        @Override
+        public void onCanceled(List<Movie> data) {
+            super.onCanceled(data);
+
+            onReleaseResources(data);
+        }
+
+        /**
+         * Handles a request to completely reset the Loader.
+         */
+        @Override
+        protected void onReset() {
+            super.onReset();
+
+            // Ensure the loader is stopped
+            onStopLoading();
+
+            // At this point we can release resources
+            if (mData != null) {
+                onReleaseResources(mData);
+                mData = null;
+            }
+        }
+
+        /**
+         * Helper function to take care of releasing resources associated with
+         * an actively loaded data set.
+         */
+        protected void onReleaseResources(List<Movie> data) {
+            // For a simple List<> there is nothing to do. For something
+            // like a Cursor, we would close it here.
+        }
+    }
+
+    private static class TraktMoviesAdapter extends ArrayAdapter<Movie> {
 
         private LayoutInflater mLayoutInflater;
 
-        private int mLayout;
+        private static final int LAYOUT = R.layout.movie_row;
 
         private ImageDownloader mImageDownloader;
 
-        public TraktMovieAdapter(Context context, int layout, List<Movie> objects) {
-            super(context, layout, objects);
+        public TraktMoviesAdapter(Context context) {
+            super(context, LAYOUT);
             mLayoutInflater = (LayoutInflater) context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            mLayout = layout;
             mImageDownloader = ImageDownloader.getInstance(context);
         }
 
@@ -159,7 +236,7 @@ public class TraktMoviesFragment extends SherlockListFragment {
             ViewHolder viewHolder;
 
             if (convertView == null) {
-                convertView = mLayoutInflater.inflate(mLayout, null);
+                convertView = mLayoutInflater.inflate(LAYOUT, null);
 
                 viewHolder = new ViewHolder();
                 viewHolder.title = (TextView) convertView.findViewById(R.id.title);
@@ -182,15 +259,24 @@ public class TraktMoviesFragment extends SherlockListFragment {
 
             return convertView;
         }
-    }
 
-    public final class ViewHolder {
+        public void setData(List<Movie> data) {
+            clear();
+            if (data != null) {
+                for (Movie userProfile : data) {
+                    add(userProfile);
+                }
+            }
+        }
 
-        public TextView title;
+        static class ViewHolder {
 
-        public TextView description;
+            public TextView title;
 
-        public ImageView poster;
+            public TextView description;
+
+            public ImageView poster;
+        }
     }
 
 }
